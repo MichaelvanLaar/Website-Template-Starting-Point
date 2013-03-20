4d2856f1922c7ee480a58bb2a1f49768971ab9a3
Michael van Laar <michael@van-laar.de>
2013-03-20 05:43:38 +0100

    Ver. 2.8: Dropdown menu now works on click
    Since :hover as trigger for pure CSS dropdown menus causes some problems
    on touch screen devices, the menu now only reacts on clicks.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ Individual JavaScript for [Project name]
 
 jQuery Scripts
 
-1. SelectNav.js initialisation
+1. Menu (dropdown menu functionality and SelectNav.js initialisation)
 2. Sisyphus initialisation
 3. enquire.js listener setup (should be the last call in this script file)
 
@@ -15,11 +15,32 @@ jQuery Scripts
 
 
 /* =============================================================================
-   1.  SelectNav.js initialisation
+   1.  Menu (dropdown menu functionality and SelectNav.js initialisation)
    ========================================================================== */
 
-enquire.register('screen and (max-width: 599px)', {
-	match: function(){},
+enquire.register('screen and (min-width: 570px)', {
+	match: function(){
+		mvl_dropdown_toggles = $('#main-navigation-list').find('.has_children').children('a');     // all <a href="…"> which are direct children of <li class="has_children">
+		$(mvl_dropdown_toggles).each(function(){
+			var mvl_dropdown_href = $(this).attr('href');
+			$(this).attr('data-target', mvl_dropdown_href).removeAttr('href');
+		});
+		$(mvl_dropdown_toggles).click(function(){
+			var mvl_dropdown_toggle_li = $(this).parent('li');                    // the <li class="has_children"> which is a direct parent of the clicked <a href="…">
+			$(mvl_dropdown_toggle_li).toggleClass('active');                      // show resp. hide clicked dropdown menu
+			$(mvl_dropdown_toggle_li).siblings('.active').removeClass('active');  // hide all dropdown menus below all siblings of the <li class="has_children"> which is a direct parent of the clicked <a href="…">
+			$(mvl_dropdown_toggle_li).find('.active').removeClass('active');      // hide all dropdown menus below clicked <a href="…">
+		});
+		$('#main-navigation-list').bind('clickoutside', function(event){
+			$(mvl_dropdown_toggles).parent('li').removeClass('active');           // hide all dropdown menus when the user clicks anywhere outside the menu area
+		});
+	},
+	unmatch: function() {
+		$(mvl_dropdown_toggles).each(function(){
+			var mvl_dropdown_href = $(this).attr('data-target');
+			$(this).attr('href', mvl_dropdown_href).removeAttr('data-target');
+		});
+	},
 	setup: function(){
 		selectnav('main-navigation-list', {
 			activeclass: 'current',
@@ -27,8 +48,7 @@ enquire.register('screen and (max-width: 599px)', {
 			indent: ' –',
 			label: '► Navigation'
 		});
-	},
-	deferSetup: true
+	}
 });
 
 
