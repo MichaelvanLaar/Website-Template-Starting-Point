e05df9502324a10596c9450a7a842e0d85bd7607
Michael van Laar <michael@van-laar.de>
2013-03-20 10:03:35 +0100

    Ver. 2.8.1: Minor improvements of new menu

--- js/script.js
+++ js/script.js
@@ -20,33 +20,44 @@ jQuery Scripts
 
 enquire.register('screen and (min-width: 570px)', {
 	match: function(){
-		mvl_dropdown_toggles = $('#main-navigation-list').find('.has_children').children('a');     // all <a href="…"> which are direct children of <li class="has_children">
-		$(mvl_dropdown_toggles).each(function(){
+		$(mvl_dropdown_toggles_a).each(function(){
 			var mvl_dropdown_href = $(this).attr('href');
 			$(this).attr('data-target', mvl_dropdown_href).removeAttr('href');
 		});
-		$(mvl_dropdown_toggles).click(function(){
-			var mvl_dropdown_toggle_li = $(this).parent('li');                    // the <li class="has_children"> which is a direct parent of the clicked <a href="…">
-			$(mvl_dropdown_toggle_li).toggleClass('active');                      // show resp. hide clicked dropdown menu
-			$(mvl_dropdown_toggle_li).siblings('.active').removeClass('active');  // hide all dropdown menus below all siblings of the <li class="has_children"> which is a direct parent of the clicked <a href="…">
-			$(mvl_dropdown_toggle_li).find('.active').removeClass('active');      // hide all dropdown menus below clicked <a href="…">
+		$(mvl_dropdown_toggles_a).click(function(){
+			var mvl_dropdown_toggle_li = $(this).parent('li');                       // the <li class="has_children"> which is a direct parent of the clicked <a href="…">
+			$(mvl_dropdown_toggle_li).toggleClass('show-submenu');                   // show resp. hide clicked dropdown menu
+			$(mvl_dropdown_toggle_li)
+				.siblings('.show-submenu')
+				.removeClass('show-submenu');                                        // hide all dropdown menus below all siblings of the <li class="has_children"> which is a direct parent of the clicked <a href="…">
+			$(mvl_dropdown_toggle_li)
+				.find('.show-submenu')
+				.removeClass('show-submenu');                                        // hide all dropdown menus below clicked <a href="…">
 		});
 		$('#main-navigation-list').bind('clickoutside', function(event){
-			$(mvl_dropdown_toggles).parent('li').removeClass('active');           // hide all dropdown menus when the user clicks anywhere outside the menu area
+			$(mvl_dropdown_toggles_li).removeClass('show-submenu');                  // hide all dropdown menus when the user clicks anywhere outside the menu area
 		});
 	},
 	unmatch: function() {
-		$(mvl_dropdown_toggles).each(function(){
+		$('#main-navigation-list').unbind('clickoutside');
+		$(mvl_dropdown_toggles_a).each(function(){
 			var mvl_dropdown_href = $(this).attr('data-target');
 			$(this).attr('href', mvl_dropdown_href).removeAttr('data-target');
 		});
 	},
 	setup: function(){
+
+		// preparation for dropdown menu
+		mvl_dropdown_toggles_li = $('#main-navigation-list').find('.has_children');  // cache all <li class="has_children">
+		mvl_dropdown_toggles_a = $(mvl_dropdown_toggles_li).children('a');             // cache all <a href="…"> which are direct children of <li class="has_children">
+
+		// SelectNav.js initialization
 		selectnav('main-navigation-list', {
 			activeclass: 'current',
 			nested: true,
 			indent: ' –',
 			label: '► Navigation'
+
 		});
 	}
 });
