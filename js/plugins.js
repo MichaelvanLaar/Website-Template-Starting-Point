5a83ddd4bb8c4e8e08c6b9a1d751307fd09341ce
Michael van Laar <michael@van-laar.de>
2016-03-05 01:15:33 +0100

    More Sass

--- js/plugins.js
+++ js/plugins.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
             css: {
                 clear: 'both'
             }
-        }).addClass('print-links-footer print-only');
+        }).addClass('print-links-footer  print-only');
         var footnotesLabel = $('<h3></h3>', {
             text: 'Links:'
         }).appendTo(footnotesWrapper);
