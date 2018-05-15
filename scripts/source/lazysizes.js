2820fd69b5e79d9dbf1f3843fb4ca8e6d9bb29c4
Michael van Laar <michael@van-laar.de>
2018-05-15 12:24:47 +0200

    docs: add missing header comments

--- scripts/source/lazysizes.js
+++ scripts/source/lazysizes.js
@@ -1,3 +1,13 @@
+/**
+ * =============================================================================
+ * lazysizes
+ * https://github.com/aFarkas/lazysizes
+ *
+ * Use build process (see “build:scripts” script in package.json) to transpile,
+ * bundle and minify this script.
+ * =============================================================================
+ */
+
 import "../../node_modules/lazysizes/plugins/print/ls.print.js";
 import * as lazySizes from "lazysizes";
 lazySizes.init();
--- scripts/source/respimage.js
+++ scripts/source/respimage.js
@@ -1,1 +1,11 @@
+/**
+ * =============================================================================
+ * respimage
+ * https://github.com/aFarkas/respimage
+ *
+ * Use build process (see “build:scripts” script in package.json) to transpile,
+ * bundle and minify this script.
+ * =============================================================================
+ */
+
 import "respimage";
